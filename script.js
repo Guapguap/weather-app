40c0f3d52d62858d4209214278940fe51fa20e4c
alberto londono <albertolondono.londono5@gmail.com>
2022-05-09 22:40:11 -0500

    added another function to display the data. When logged with the desired argument, everything shows up except the icon but that will be added later. Going to create an eventlistener for the search bar

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ let weather = {
 apiKey: '4541b3868e8909ef309a6e0a539cf01f',
 
 fetchWeather: function (city) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + this.apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
     .then((response) => response.json())
     .then((data) => this.displayWeather(data));
+    
 },
 
-
+displayWeather: function(data){
+    
+    let {name} = data;
+    let {icon, description} = data.weather[0];
+    let {temp, humidity} = data.main;
+    let {speed} = data.wind;
+    console.log(name,icon,description,temp,humidity,speed);
+    document.querySelector('.city').innerText = "Weather in " + name;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = temp + "°F";
+    document.querySelector('.humid').innerText = "Humidity: " + humidity + "%";
+    document.querySelector('.wind').innerText = "Wind Speed: " + speed + "mph";
+}
 };
 
 
+document.querySelector
